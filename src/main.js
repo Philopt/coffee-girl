77ca704215f40116dc0b645fe337764536d8e8b0
Philopt <agtxenon@gmail.com>
2025-07-02 16:52:44 -0700

    fix dog death state

--- src/main.js
+++ src/main.js
@@ -3067,6 +3067,8 @@ function dogsBarkAtFalcon(){
                 },
                 onComplete: () => {
                   ensureOnGround(dog);
+                  dog.attacking = false;
+                  dog.dead = true;
                   if(done) done();
                 }
               });
